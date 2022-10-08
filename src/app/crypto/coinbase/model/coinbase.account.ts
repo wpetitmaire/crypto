8a10e5489f917def1fb7de7e58fa64a3f4f19092
Willy PM <w.petitmaire@gmail.com>
2022-10-08 10:08:36 +0200

    css libel crypto

--- src/app/crypto/coinbase/model/coinbase.account.ts
+++ src/app/crypto/coinbase/model/coinbase.account.ts
@@ -2,10 +2,23 @@ export class CoinbaseAccount {
   id: string;
   code: string;
   libel: string;
+  balance: number;
+  price: number;
+  exponent: number;
 
-  constructor(id: string, code: string, libel: string) {
+  constructor(
+    id: string,
+    code: string,
+    libel: string,
+    balance: number,
+    price: number,
+    exponent: number
+  ) {
     this.id = id;
     this.code = code;
     this.libel = libel;
+    this.balance = balance;
+    this.price = price;
+    this.exponent = exponent;
   }
 }
